8f3016d0f05bc4872be03c86b7c04ad2bce39bcf
lee <ljm@it-innovation.soton.ac.uk>
2015-08-21 16:55:27 +0100

    added some large buttons to the top of the dashboard

--- anello/dashboard/views.py
+++ anello/dashboard/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Query
+
+import json
 
 def home_page(request):
-  return render(request, 'home.html')
+  data = Query.objects.all()[0]
+  cards = json.loads(data.payload)
+  done_items = [v for k,v in cards.items() if v['list']=='done']
+  month_items = []
+  for k,v in cards.items():
+    history = [hi[0] for hi in v['history']]
+    if 'this month' in history or 'this week' in history or 'do today' in history or 'in progress' in history:
+      month_items.append(v)
+
+
+  return render(request, 'home.html', {'number_completed':len(done_items), 'number_thismonth':len(month_items)})
